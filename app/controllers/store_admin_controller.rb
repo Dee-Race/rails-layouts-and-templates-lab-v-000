792536f7b988f3b6682f2bfef52ebe519de70809
Dee Race <deerace12@gmail.com>
2018-06-25 17:39:30 +0000

    automatically backed up by learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -2,8 +2,16 @@ class StoreAdminController < ApplicationController
 
   layout "admin"
 
-  def home 
+  def home
 
+  end
+
+  def orders
+    render :layout => "order_administration"
+  end 
+
+  def invoice
+    render :layout => false 
   end 
 
   
